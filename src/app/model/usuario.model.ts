f6ae976dab8d6df73b990b85a1cac91d3b84ba72
Kadson Jader <kadsonjader@Kadsons-iMac.local>
2020-03-24 12:23:19 -0300

    create user service

---
+++ src/app/model/usuario.model.ts
@@ -0,0 +1,8 @@
+export class Usuario {
+  constructor(
+    public id: string,
+    public email: string,
+    public senha: string,
+    public perfil: string
+  ){}
+}
---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,37 @@
+import { HELP_DESK_API } from './helpdesk.api';
+import { Usuario } from './../model/usuario.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(private http: HttpClient) {}
+
+  login(usuario: Usuario){
+    return this.http.post(`${HELP_DESK_API}/api/auth`, usuario);
+  }
+
+  createOrUpdate(usuario: Usuario){
+    if(usuario.id != null && usuario.id != ''){
+      return this.http.put(`${HELP_DESK_API}/api/user`,usuario);
+    }else{
+      usuario.id = null;
+      return this.http.post(`${HELP_DESK_API}/api/user`,usuario);
+    }
+  }
+
+  findAll(page: number, count:number){
+    return this.http.get(`${HELP_DESK_API}/api/user/${page}/${count}`);
+  }
+
+  findById(id: string){
+    return this.http.get(`${HELP_DESK_API}/api/user/${id}`);
+  }
+
+  delete(id: string){
+    return this.http.delete(`${HELP_DESK_API}/api/user/${id}`);
+  }
+}
